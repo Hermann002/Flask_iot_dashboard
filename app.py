71017c7c275c98c7c76857f10f09c86e5f44ba3f
Hermann002 <hermannnzeudeu@gmail.com>
2023-05-03 15:30:11 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,54 @@
+# Import packages
+from dash import Dash, html, dash_table, dcc, callback, Output, Input
+import pandas as pd
+import plotly.express as px
+import dash_bootstrap_components as dbc
+from database import connect
+
+# Incorporate data
+df = pd.DataFrame(connect())
+df.columns =['idcapteur','temperature','humidity']
+
+# Initialize the app - incorporate a Dash Bootstrap theme
+external_stylesheets = [dbc.themes.CERULEAN]
+app = Dash(__name__, external_stylesheets=external_stylesheets)
+
+
+
+# App layout
+app.layout = dbc.Container([
+    dbc.Row([
+        html.Div('My First App with Data, Graph, and Controls', className="text-primary text-center fs-3")
+    ]),
+
+    # dbc.Row([
+    #     dbc.RadioItems(options=[{"label": x, "value": x} for x in ['pop', 'lifeExp', 'gdpPercap']],
+    #                 value='lifeExp',
+    #                 inline=True,
+    #                 id='radio-buttons-final')
+    # ]),
+
+    dbc.Row([
+        dbc.Col([
+            dash_table.DataTable(data=df.to_dict('records'), page_size=10, style_table={'overflowX': 'auto'})
+        ], width=6),
+
+        dbc.Col([
+            dcc.Graph(figure=px.line(df, x='idcapteur', y=['temperature','humidity']), id='my-first-graph-final')
+        ], width=6),
+    ]),
+
+], fluid=True)
+
+# Add controls to build the interaction
+# @callback(
+#     Output(component_id='my-first-graph-final', component_property='figure'),
+#     Input(component_id='radio-buttons-final', component_property='value')
+# )
+# def update_graph(col_chosen):
+#     fig = px.histogram(df, x='IdCapteur', y=['temperature','humidity'], barmode= 'group')
+#     return fig
+
+# Run the app
+if __name__ == '__main__':
+    app.run_server(debug=True)
